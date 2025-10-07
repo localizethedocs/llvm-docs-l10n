80e92dbaa4ba193ba7769eaa094c1850e7e4f05b
Haowei Hsu <hwhsu1231@localizethedocs.org>
2025-10-08 01:30:50 +0800

    ✨ feat(init): initialize branch 'main'
    Initialized the 'main' branch with the following files:
    
    - Added document files: README.md, LICENSE-BSD, LICENSE-CC.
    - Added GitHub workflow files: ci-check-pr-status,..., etc.
    - Added GitHub issue templates: language_request.yml,..., etc.
    - Added Git config files: .gitmodules, .gitignore.
    - Added Git submodules: cmake/modules, cmake/plugins.
    - Added CMake script files: CMakeLists.txt, cmake/**/* files.
    - Added config files: versions.json, languages.json, crowdin.yml.

---
+++ cmake/custom/ltd-config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+    ["21",      "Release 21"],
+    ["20",      "Release 20"],
+    ["19",      "Release 19"],
+    ["18",      "Release 18"],
+    ["17",      "Release 17"],
+    ["16",      "Release 16"],
+    ["15",      "Release 15"],
+    ["14",      "Release 14"],
+    ["13",      "Release 13"],
+    ["12",      "Release 12"],
+    ["11",      "Release 11"],
+    ["10",      "Release 10"],
+    ["9",       "Release 9"],
+    ["8",       "Release 8"],
+    ["7",       "Release 7"],
+    ["6",       "Release 6"],
+    ["5",       "Release 5"],
+    ["4",       "Release 4"],
+    ["3",       "Release 3"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/llvm-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/llvm-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/llvm-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/llvm-docs-l10n"],
+  ]
+};
