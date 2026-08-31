9091df4293b42af6ab5d019b0ba897610d381f8a
ltdrobot <ltdrobot@localizethedocs.org>
2026-08-31 17:27:15 +0000

    Deploying to pages from @ localizethedocs/llvm-docs-l10n@1144b3ce5b024deaa9e4fe6313b600ca30378586 🚀

---
+++ ltd-config.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["fr-fr",   "Français"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["main",    "Development"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/llvm-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/llvm-docs-l10n"],
+    ["GitCode", "https://gitcode.com/localizethedocs/llvm-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/llvm-docs-l10n"],
+  ]
+};
